7d2c0fe3c4b76f244f2860a214f842a39fea46bf
LiangboKent <1245446500@qq.com>
2016-05-11 11:58:14 +0800

    Create python_package.sh

---
+++ python_package.sh
@@ -0,0 +1,24 @@
+#basic
+pip install -U shutil;
+pip install -U python-dateutil
+#db
+pip install -U pymongo;
+#search
+pip install -U elasticsearch;
+#cache
+pip install -U redis;
+#crawler
+pip install -U scrapy;
+#nlp
+pip install -U jieba;
+#server
+pip install -U cherrypy;
+#data analysis
+pip install numpy;
+pip install -U scipy;
+pip install -U matplotlib;
+
+
+
+
+
